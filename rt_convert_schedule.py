fc577996cb413c095fa9aac213bb57c22fa4444f
yasaka999 <hanxiong@gmail.com>
2022-04-09 11:25:17 +0800

    修改为使用in_close_write事件

--- rt_convert_schedule.py
+++ rt_convert_schedule.py
@@ -8,6 +8,7 @@ from pyinotify import (
     IN_ACCESS,
     IN_ATTRIB,
     IN_CREATE,
+    IN_CLOSE_WRITE,
     IN_DELETE,
     IN_MODIFY,
     Notifier,
@@ -17,25 +18,25 @@ from pyinotify import (
 
 
 class EventHandler(ProcessEvent):
-    def process_IN_CREATE(self, event):
+    def process_IN_CLOSE_WRITE(self, event):
         print ("%s: Create file: %s " %(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), os.path.join(event.path, event.name)))
         src_name = event.name
         if src_name.endswith(".txt"):
             map2 = mapping(map_file)
             if map2.has_key(src_name.split(".")[0]):
-                dst_name = map2[src_name.split(".")[0]]
-                print(src_name)
-                print(dst_name + ".txt")
-                shutil.copy(src_dir + "/" + src_name, dst_dir + "/" + dst_name + ".txt")
-                shutil.copy(src_dir + "/" + src_name, dst_dir + "/" + dst_name + ".txt.ok")
+                dst_name = map2[src_name.split(".")[0]]+".txt"
+                shutil.copy(src_dir + "/" + src_name, dst_dir + "/" + dst_name)
+                shutil.copy(src_dir + "/" + src_name, dst_dir + "/" + dst_name + ".ok")
+                os.chmod(dst_dir + "/" + dst_name, 0777)
+                os.chmod(dst_dir + "/" + dst_name + ".ok", 0777)
                 shutil.move(src_dir + "/" + src_name, bak_dir + "/" + src_name)
                 print ("%s: Convert %s to %s sucess" %(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()),src_name,dst_name))
             else:
-                print("channel not exist, please add to map.txt")
-    
-def FSMonitor(path="."):
+                shutil.move(src_dir + "/" + src_name, bak_dir + "/" + src_name)
+                print("ERROR: channel:%s not exist, please add to map.txt" %src_name.split(".")[0])
+def FSMonitor(path="."):  
     wm = WatchManager()
-    mask = IN_DELETE | IN_CREATE | IN_MODIFY | IN_ACCESS | IN_ATTRIB
+    mask = IN_DELETE | IN_CREATE | IN_MODIFY | IN_ACCESS | IN_ATTRIB | IN_CLOSE_WRITE
     notifier = Notifier(wm, EventHandler())
     wm.add_watch(path, mask, rec=True)
     print "now starting monitor %s" % (path)
